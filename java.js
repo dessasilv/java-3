3c1f08f05e7779c1f1bcecae2a5fadce8c10a86d
dessasilv <andressasilva1423ag@gmail.com>
2023-05-18 10:14:07 -0300

    .

--- java.js
+++ java.js
@@ -5,22 +5,36 @@ var nota3 = Number(window.prompt('terceira nota'))
 
 var media = ((nota1 + nota2 + nota3)/3)//questão 5
 
-var num1 =parseInt(window.prompt('coloque um numero: '))
-var num2 =parseInt(window.prompt('coloque outro numero: '))
-var cod = parseInt(window.prompt('digite 1, 2 ou 3: ')) // questão 6
-
 if(media >= 8){
-    window.alert(`você foi aprovado, sua media é:  ${media}`)
+    window.alert (`você foi aprovado, sua media é:  ${media}`)
 } else{
     window.alert(`você foi reprovado, sua media é:  ${media}`)
 }
+
+
+var num1 =parseInt(window.prompt('coloque um numero: '))
+var num2 =parseInt(window.prompt('coloque outro numero: '))
+var cod = parseInt(window.prompt('digite 1 (pra soma), 2 (pra multiplicação) ou 3(para divisão): ')) // questão 6
 
 
 if(cod ==1 ){
     document.write( 'soma: ', num1 + num2)
 } else if (cod == 2){
     document.write(' multiplicação: ', num1 * num2)
-} else  {
+} else if(cod == 3) {
     document.write(' divisão:', num1/num2)
+} else {
+    document.write (`tente novamente`)
 }
 
+ switch (cod) {
+    case (cod == 1):
+        document.write( 'soma: ', num1 + num2);
+        break;
+    case (cod == 2):
+        document.write(' multiplicação: ', num1 * num2);
+        break;
+    case (cod == 3) :
+        document.write(' divisão:', num1/num2)
+        break;
+ }
